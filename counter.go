5dee13e23aa28ec31e80eb3f5f2e88b1c2fbf79e
matt maier <maier@users.noreply.github.com>
2016-06-06 15:39:36 -0400

    remove commented code blocks

--- counter.go
+++ counter.go
@@ -36,35 +36,3 @@ func (m *CirconusMetrics) RemoveCounterFunc(metric string) {
 	defer m.cfm.Unlock()
 	delete(m.counterFuncs, metric)
 }
-
-/*
-type Counter string
-
-// Add increments the counter by one.
-func (c Counter) Add() {
-	c.AddN(1)
-}
-
-// AddN increments the counter by N.
-func (c Counter) AddN(delta uint64) {
-	cm.Lock()
-	defer cm.Unlock()
-	counters[string(c)] += delta
-}
-
-// SetFunc sets the counter's value to the lazily-called return value of the
-// given function.
-func (c Counter) SetFunc(f func() uint64) {
-	cm.Lock()
-	defer cm.Unlock()
-	counterFuncs[string(c)] = f
-}
-
-// Remove removes the given counter.
-func (c Counter) Remove() {
-	cm.Lock()
-	defer cm.Unlock()
-	delete(counters, string(c))
-	delete(counterFuncs, string(c))
-}
-*/
