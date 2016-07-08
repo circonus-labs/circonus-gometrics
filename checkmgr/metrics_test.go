fd37e742d1b268f54a2d4e5cc6d4bf25063d101f
matt maier <maier@users.noreply.github.com>
2016-07-08 15:38:22 -0400

    add inventoryMetric test

--- checkmgr/metrics_test.go
+++ checkmgr/metrics_test.go
@@ -2,6 +2,8 @@ package checkmgr
 
 import (
 	"testing"
+
+	"github.com/circonus-labs/circonus-gometrics/api"
 )
 
 func TestIsMetricActive(t *testing.T) {
@@ -12,6 +14,39 @@ func TestIsMetricActive(t *testing.T) {
 	cm.activeMetrics = map[string]bool{
 		"foo": true,
 	}
+
+	t.Log("Testing for 'foo', foo in list")
+	if !cm.IsMetricActive("foo") {
+		t.Error("Expected true")
+	}
+
+	t.Log("Testing for 'bar', bar not in list")
+	if cm.IsMetricActive("bar") {
+		t.Error("Expected false")
+	}
+}
+
+func TestInventoryMetrics(t *testing.T) {
+	t.Log("Testing correct return from IsMetricActive")
+
+	cm := &CheckManager{}
+	cm.checkBundle = &api.CheckBundle{}
+	cm.checkBundle.Metrics = []api.CheckBundleMetric{
+		api.CheckBundleMetric{
+			Name:   "foo",
+			Type:   "text",
+			Status: "active",
+		},
+	}
+
+	cm.activeMetrics = make(map[string]bool)
+	t.Log("Testing for 'foo', foo not in list")
+	if cm.IsMetricActive("foo") {
+		t.Error("Expected false")
+	}
+
+	t.Log("Inventory metrics in check bundle")
+	cm.inventoryMetrics()
 
 	t.Log("Testing for 'foo', foo in list")
 	if !cm.IsMetricActive("foo") {
