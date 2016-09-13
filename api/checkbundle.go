7076b67eea9d727a1ea07f0a8e2d9bc63ff94740
matt maier <maier@users.noreply.github.com>
2016-09-13 17:00:49 -0400

    add support for non-httptrap check types

--- api/checkbundle.go
+++ api/checkbundle.go
@@ -7,6 +7,7 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 // CheckBundleConfig configuration specific to check type
@@ -14,6 +15,13 @@ type CheckBundleConfig struct {
 	AsyncMetrics  bool   `json:"async_metrics"`
 	Secret        string `json:"secret"`
 	SubmissionURL string `json:"submission_url"`
+	ReverseSecret string `json:"reverse:secret_key"`
+	HTTPVersion   string `json:"http_version,omitempty"`
+	Method        string `json:"method,omitempty"`
+	Payload       string `json:"payload,omitempty"`
+	Port          string `json:"port,omitempty"`
+	ReadLimit     string `json:"read_limit,omitempty"`
+	URL           string `json:"url,omitempty"`
 }
 
 // CheckBundleMetric individual metric configuration
@@ -32,7 +40,7 @@ type CheckBundle struct {
 	Created            int                 `json:"_created,omitempty"`
 	LastModified       int                 `json:"_last_modified,omitempty"`
 	LastModifedBy      string              `json:"_last_modifed_by,omitempty"`
-	ReverseConnectUrls []string            `json:"_reverse_connection_urls,omitempty"`
+	ReverseConnectUrls []string            `json:"_reverse_connection_urls"`
 	Brokers            []string            `json:"brokers"`
 	Config             CheckBundleConfig   `json:"config"`
 	DisplayName        string              `json:"display_name"`
@@ -61,7 +69,21 @@ func (a *API) FetchCheckBundleByCID(cid CIDType) (*CheckBundle, error) {
 	}
 
 	checkBundle := &CheckBundle{}
-	json.Unmarshal(result, checkBundle)
+	if err := json.Unmarshal(result, checkBundle); err != nil {
+		return nil, err
+	}
+
+	if checkBundle.Type != "httptrap" {
+		if len(checkBundle.ReverseConnectUrls) == 0 {
+			return nil, fmt.Errorf("%s is not an HTTPTRAP check and no reverse connection urls found", checkBundle.Checks[0])
+		}
+		// we need to build a submission_url for non-httptrap checks
+		mtevURL := checkBundle.ReverseConnectUrls[0]
+		// mtev_reverse://50.31.170.148:43191/check/8d23721a-8e7b-4ae8-fb1d-dffe895c9dcb
+		mtevURL = strings.Replace(mtevURL, "mtev_reverse", "https", 1)
+		mtevURL = strings.Replace(mtevURL, "check", "module/httptrap", 1)
+		checkBundle.Config.SubmissionURL = fmt.Sprintf("%s/%s", mtevURL, checkBundle.Config.ReverseSecret)
+	}
 
 	return checkBundle, nil
 }
