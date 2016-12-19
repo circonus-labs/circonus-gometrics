6eb2c2929c1640729d613fe4d8deb5aa3c6be765
matt maier <maier@users.noreply.github.com>
2016-12-19 18:49:27 -0500

    upd: add more empirical testing for tags

--- checkmgr/metrics_test.go
+++ checkmgr/metrics_test.go
@@ -5,6 +5,7 @@
 package checkmgr
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/circonus-labs/circonus-gometrics/api"
@@ -182,32 +183,63 @@ func TestAddMetricTags(t *testing.T) {
 
 	t.Log("metric named 'bar', add tag")
 	{
+
 		cm.metricTags = make(map[string][]string)
 		// append, zero current
 		if !cm.AddMetricTags("bar", []string{"cat:tag"}, true) {
 			t.Fatalf("Expected true")
 		}
-		if cm.metricTags["bar"][0] != "cat:tag" {
-			t.Fatalf("expected cat:tag")
+		expected := make(map[string][]string)
+		expected["bar"] = []string{"cat:tag"}
+
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
 		}
+
 		// tag already exists, no need to add
 		if cm.AddMetricTags("bar", []string{"cat:tag"}, true) {
 			t.Fatalf("Expected false")
 		}
+
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
+		}
+
 		// append, zero tags
 		if cm.AddMetricTags("bar", []string{}, true) {
 			t.Fatalf("Expected false")
+		}
+
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
 		}
 	}
 
 	t.Log("metric named 'baz', add tag")
 	{
 		cm.metricTags = make(map[string][]string)
+
+		// append, current tag, should be noupdate
+		if cm.AddMetricTags("baz", []string{"cat1:tag1"}, true) {
+			t.Fatalf("Expected false")
+		}
+
+		if _, found := cm.metricTags["baz"]; found {
+			t.Fatalf("expected not found")
+		}
+
 		// append, one current
 		if !cm.AddMetricTags("baz", []string{"cat2:tag2"}, true) {
 			t.Fatalf("Expected true")
 		}
-		// append, tag already exists
+		expected := make(map[string][]string)
+		expected["baz"] = []string{"cat1:tag1", "cat2:tag2"}
+
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
+		}
+
+		// append, tag already exists (should be noupdate)
 		if cm.AddMetricTags("baz", []string{"cat2:tag2"}, true) {
 			t.Fatalf("Expected false")
 		}
@@ -215,28 +247,57 @@ func TestAddMetricTags(t *testing.T) {
 
 	t.Log("metric named 'foo', set tag")
 	{
+		expected := make(map[string][]string)
 		cm.metricTags = make(map[string][]string)
 
 		// set tag
 		if !cm.AddMetricTags("foo", []string{"cat:tag"}, false) {
 			t.Fatalf("Expected true")
 		}
+		expected["foo"] = []string{"cat:tag"}
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
+		}
+
 		// set, reset (pass 0 tags)
 		if !cm.AddMetricTags("foo", []string{}, false) {
 			t.Fatalf("Expected true")
 		}
+
+		if _, found := cm.metricTags["foo"]; found {
+			t.Fatal("expected not found")
+		}
+
 		// set tag
 		if !cm.AddMetricTags("foo", []string{"cat:tag"}, false) {
 			t.Fatalf("Expected true")
 		}
+
+		expected["foo"] = []string{"cat:tag"}
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
+		}
+
 		// set, no update (same tag)
 		if cm.AddMetricTags("foo", []string{"cat:tag"}, false) {
 			t.Fatalf("Expected false")
 		}
+
+		expected["foo"] = []string{"cat:tag"}
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
+		}
+
 		// replace any existing
 		if !cm.AddMetricTags("foo", []string{"cat:newtag"}, false) {
 			t.Fatalf("Expected true")
 		}
+
+		expected["foo"] = []string{"cat:newtag"}
+		if !reflect.DeepEqual(cm.metricTags, expected) {
+			t.Fatalf("expected %v got %+v", expected, cm.metricTags)
+		}
+
 	}
 }
 
